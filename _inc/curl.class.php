98b5473d52282a045654d8bd00df0a536408b546
Daddo172 <154556279+Daddo172@users.noreply.github.com>
2025-05-30 18:06:33 +0200

    Create curl.class.php

---
+++ _inc/curl.class.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!class_exists('CurlServer')){
+    class CurlServer{
+    public function post_request($url, $post_data, $access_token)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: ' . $access_token,
+            'Content-Type: application/x-www-form-urlencoded'
+        ]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($server_output);
+    }
+
+    public function get_request($url, $token)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $token
+        ]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($server_output);
+    }
+}
+}
